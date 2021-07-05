da51f49f5f6e3c05b040c8bcd5311b2ceea2d9b2
Matthew Lam <47630156+matthewlam17@users.noreply.github.com>
2021-07-05 23:23:59 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '9efeb5c811ba40e28ae09012b615ae8d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '林唯哲-个人作品集',
+  domain: 'https://www.notion.so/9efeb5c811ba40e28ae09012b615ae8d',
+  author: 'Matthew Lam',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  phonenumber: '13417070134',
+  weibo: 'matthew_lam ',
+  wechat: 'Matthew--Lam',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
